152d38689799d6aede0f77f7002f59fa0d93e3c8
ibrahim <ibrahimkerimli721@gmail.com>
2026-01-08 20:55:54 +0400

    classwork4

--- src/classwork_3/Classwork_3.java
+++ src/classwork_3/Classwork_3.java
@@ -1,5 +1,18 @@
 package classwork_3;
 
 public class Classwork_3 {
+	public static void main(String[] args) {
+		int sleeptime = 3;
+		
+		if (sleeptime < 6) {
+			System.out.println("Мало спал, пора отдохнуть!");
+		}else if (sleeptime >= 6 && sleeptime <= 8) {
+			System.out.println("Нормально, бодрый день");
+		}else if (sleeptime >= 8 && sleeptime <= 9) {
+			System.out.println("Нормально, бодрый день");
+		}else {
+			System.out.println("Переспал, пора вставать!");
+		}
 
+	}
 }
